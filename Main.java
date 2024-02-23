c33810be9ca480fb8793d42b538f417e144eac57
Pruklad <mihalcukvasa01@gmail.com>
2024-02-23 12:50:27 +0200

    create project(not full)

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+    int a=27;
+    int b=34;
+    int n=700;
+    int c=a*n;
+    int d=550;
+
+        System.out.println("\n Ціна персонального комп'ютера для роботи на данний час коливається в межах"+ n
+        + '\u0024'+ "або близько " + d + '\u20AC'+". В національній валюті це близько"+ c + "грн.");
+    }
+}
