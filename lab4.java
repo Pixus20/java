a7898f0d8e426440ba498c229ea81d6f21bb2a58
Pruklad <mihalcukvasa01@gmail.com>
2024-03-15 12:41:25 +0200

    rewrite project

--- lab4.java
+++ lab4.java
@@ -1,32 +1,36 @@
 import java.awt.*;
+import javax.swing.*;
 
-import javax.swing.JLabel;
-
-
-public class lab4 {
-    program m = new program("Program windov");
+public class lab4{
+    public static void main(String[] args) {
+        new Program("Program window");
+    }
 }
 
-class program extends javax.swing.JFrame{
-    public void paint (Graphics rys){
-        rys.setFont(new Font("Serif", Font.CENTER_BASELINE,30));
-        rys.drawString("Лбораторна робота №4",250, 100);
-        rys.setFont(new Font("Verdana", Font.ITALIC,37));
-        rys.setColor(Color.MAGENTA);
-        rys.drawString("\"Створення віконного інтерфейсу \"",250, 100);
-    }
-    public program (String str){
+class Program extends JFrame {
+    public Program(String str) {
         super(str);
-        setSize(800,600);
+        setSize(950, 600);
         setVisible(true);
-        Container cont =getContentPane();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Container cont = getContentPane();
         cont.setLayout(new FlowLayout());
-        JLabel label1 = new JLabel("Виконав");
-        label1.setToolTipText("ст. гр. ПОс-11 Михальчук В.А");
-        cont.add (label1);
-        JLabel label2 = new JLabel("Перевірив:");
-        label2.setToolTipText("Кабак В. В.");
-        cont.add (label2);
+
+        JLabel label1 = new JLabel("Виконав: ст. гр. ПОс-11 Михальчук В.А");
+        cont.add(label1);
+
+        JLabel label2 = new JLabel("Перевірив: Кабак В. В.");
+        cont.add(label2);
+    }
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setFont(new Font("Serif", Font.PLAIN, 30));
+        g.drawString("Лабораторна робота №4", 250, 100);
+
+        g.setFont(new Font("Verdana", Font.ITALIC, 37));
+        g.setColor(Color.MAGENTA);
+        g.drawString("\"Створення віконного інтерфейсу\"", 150, 150);
     }
 }
-
