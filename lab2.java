dcd7f23ea640abfab30bb078443dd48df7983c9e
Pruklad <mihalcukvasa01@gmail.com>
2024-03-15 11:53:48 +0200

    full 2-nd lab

--- lab2.java
+++ lab2.java
@@ -6,6 +6,12 @@ public class lab2 {
         int result= suma(10, 45);
         System.out.println("Сума чисел"+ result);
         IF.myIF();
+        String name="Іван";
+        for (int i=0; i<name.length(); i++){
+            char napus =name.charAt(i);
+            System.out.println(napus);
+        }
+        Static.method(42);
     }
     static void newmet1(){
         int a=12;
@@ -24,6 +30,19 @@ public class lab2 {
     class IF {
         static void myIF(){
             System.out.println("Метод пропасується з вказання класу, до якого він"+"відноситься");
+        }
+    }
+    class Static{
+        static int l=3;
+        static int m;
+        static void method(int n){
+            System.out.println("x=" +n);
+            System.out.println("l=" +l);
+            System.out.println("m=" +m);
+        }
+        static{
+            System.out.println("Static-блок ініціалізовано:");
+            m=l*4;
         }
     }
 }
