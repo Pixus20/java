abc2d70bdd894fe361e508f0d08b75bb1026d5f8
Pruklad <mihalcukvasa01@gmail.com>
2024-04-19 12:42:32 +0300

    start create applet

---
+++ lab7/lab7.java
@@ -0,0 +1,13 @@
+import java.applet.*;
+import java.awt.*;
+
+public class lab7 extends Applet {
+    public void paint(Graphics g){
+        g.drawString("Міністерство освіти та науки України", 10, 40);
+        g.drawString("Кафедра цифрових освітгніх  технологій", 10, 60);
+        g.drawString("Лабораторна робота на тему: 'Створення аплетів'", 10, 80);
+        g.drawString("Виконав Михальчук Василь ", 10, 100);
+    }
+    public void init(){
+    }
+}
